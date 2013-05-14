63bf5abcb66357ecded9f6885c92be833e8345c8
wuboy0307 <wuboys@gmail.com>
2013-05-14 18:02:56 +0800

    test replace render_original

--- app/overrides/product_image_zoom.rb
+++ app/overrides/product_image_zoom.rb
@@ -4,5 +4,5 @@
 Deface::Override.new(:virtual_path => "spree/products/_image",
                       :name => "product-image-zoom",
                       :surround => "code[erb-loud]:contains('product_image')") do
-                        "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= render_original %></a><div class='clearfix'></div>"
+                        "<a href='<%= @product.images.empty? ? \"javascript:void(0);\" : @product.images.first.attachment.url(:original) %>' rel='product-gallery' class='<%= @product.images.empty? ? \"javascript:void(0);\" : \"spree_jqzoom\" %>'><%= image_tag(image.attachment.url(:origin), :itemprop => "image") %></a><div class='clearfix'></div>"
                       end
