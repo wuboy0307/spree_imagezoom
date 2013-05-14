42106bfab2a98fe01a60a755c3b2426e4085e7f4
wuboy0307 <wuboys@gmail.com>
2013-05-14 17:45:55 +0800

    fix ie version bug

--- app/assets/javascripts/store/spree_imagezoom.js
+++ app/assets/javascripts/store/spree_imagezoom.js
@@ -19,9 +19,24 @@
  *
  * Date: 03 May 2011 22:16:00
  */
+ 
+function getInternetExplorerVersion()
+// Returns the version of Internet Explorer or a -1
+// (indicating the use of another browser).
+{
+    var rv = -1; // Return value assumes failure.
+    if (navigator.appName == 'Microsoft Internet Explorer')
+    {
+        var ua = navigator.userAgent;
+        var re  = new RegExp("MSIE ([0-9]{1,}[\.0-9]{0,})");
+    if (re.exec(ua) != null)
+        rv = parseFloat( RegExp.$1 );
+    }
+    return rv;
+} 
 (function ($) {
     //GLOBAL VARIABLES
-    var isIE6 = ($.browser.msie && $.browser.version < 7);
+    var isIE6 = getInternetExplorerVersion() == 6;
     var body = $(document.body);
     var window = $(window);
     var jqzoompluging_disabled = false; //disabilita globalmente il plugin
